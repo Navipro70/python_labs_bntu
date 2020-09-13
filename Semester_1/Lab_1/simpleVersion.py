4e81b5328954662fc1d5837b4f8c2a496503ac2d
Dmitry <dima.matatoff@yandex.ru>
2020-09-13 21:01:17 +0300

    Make 1st lab in simple version

---
+++ Semester_1/Lab_1/simpleVersion.py
@@ -0,0 +1,23 @@
+# This program is first lab in BNTU, designed to show basic capabilities of python.
+# Version 1.0
+# Full name: Matatov Dmitry Valer'evich, ФИО: Мататов Дмитрий Валерьевич
+# Group number: № 10702120
+# Date of development: 13 september, 2020
+
+print('\n\n"Безнадёжно — это когда на крышку гроба падает земля. Остальное можно исправить. — Джейсон Стэтхэм"'
+      '\n"Hopeless is when the earth falls on the lid of the coffin. The rest can be fixed. — Jason Statham"\n\n')
+print("#########\t      #       \t#       #\t#######")
+print("#        \t     # #      \t# #   # #\t#      ")
+print("#        \t    #   #     \t#  # #  #\t#######")
+print("#  ######\t   ########   \t#   #   #\t#      ")
+print("#       #\t  #        #  \t#       #\t#      ")
+print("#       #\t #          # \t#       #\t#      ")
+print("#########\t#            #\t#       #\t#######\n\n")
+
+print("#########\t#           #\t#######\t####### ")
+print("#       #\t #         # \t#      \t#      #")
+print("#       #\t  #       #  \t#######\t#      #")
+print("#       #\t   #     #   \t#      \t####### ")
+print("#       #\t    #   #    \t#      \t# #     ")
+print("#       #\t     # #     \t#      \t#   #   ")
+print("#########\t      #      \t#######\t#     # ")
